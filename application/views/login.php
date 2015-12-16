2e67312284fb12422d9fe9a85e790842c1a5d0ec
ImadSalimi <salimi.imad@gmail.com>
2015-12-16 23:44:18 +0000

    Centered login form

--- application/views/login.php
+++ application/views/login.php
@@ -1,12 +1,12 @@
 <div id="login" class="container">
-	<?= form_open("Users/login") ?> 
-		<div class="form-group col-sm-3">
-			<label class="col-sm-1" for="username">Login</label>
-			<input type="text" class="form-control">
+	<?= form_open("Users/login", 'class="col-sm-4 col-sm-offset-4"') ?> 
+		<div class="form-group">
+			<label for="username">Login</label>
+			<input type="text" name="username" class="form-control">
 		</div>
-		<div class="form-group col-sm-3">
-			<label class="col-sm-1" for="username">Login</label>
-			<input type="text" class="form-control">
+		<div class="form-group">
+			<label for="password">Mot de Passe</label>
+			<input type="password" name="password" class="form-control">
 		</div>
 	</form>
 </div>
