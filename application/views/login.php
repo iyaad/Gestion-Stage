77aadfde27afc4b5756403764898e5f4834e58b1
ImadSalimi <salimi.imad@gmail.com>
2015-12-16 23:37:17 +0000

    Fixed login view

--- application/views/login.php
+++ application/views/login.php
@@ -1,14 +1,12 @@
-<div id="login">
-
+<div id="login" class="container">
 	<?= form_open("Users/login") ?> 
-		<div class="form-group">
-			<div class="col-sm-2"><label >login</label></div>
-			<div class="col-sm-3"><input type="password" class="form-group" value="ici.."></div>
+		<div class="form-group col-sm-3">
+			<label class="col-sm-1" for="username">Login</label>
+			<input type="text" class="form-control">
 		</div>
-		<div class="form-group">
-			<div class="col-sm-2"><label >password</label></div>
-			<div class="col-sm-3"><input type="password" class="form-group" value="ici.."></div>
+		<div class="form-group col-sm-3">
+			<label class="col-sm-1" for="username">Login</label>
+			<input type="text" class="form-control">
 		</div>
-		<div class="col-sm">
 	</form>
 </div>
