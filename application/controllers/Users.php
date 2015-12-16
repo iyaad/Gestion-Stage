3b903b97fe930a53b84424139b718dc443e38e6d
ImadSalimi <salimi.imad@gmail.com>
2015-12-16 23:19:35 +0000

    Included Ubold css files

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -2,11 +2,17 @@
 
 class Users extends MY_Controller {
 
-	public function login(){
-
-		$data['title'] = "login";
-		$this->render();
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->library('form_validation');
 	}
 
+	public function login()
+	{
+		$data['title'] = 'Login';
+		$this->render('login', $data);
+	}
 
 }
--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="<?= asset_url('css/bootstrap.css') ?>">
+	<link rel="stylesheet" href="<?= asset_url('css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" href="<?= asset_url('css/bootstrap-theme.min.css') ?>">
+	<link rel="stylesheet" href="<?= asset_url('css/core.css') ?>">
+	<link rel="stylesheet" href="<?= asset_url('css/icons.css') ?>">
+	<link rel="stylesheet" href="<?= asset_url('css/pages.css') ?>">
+	<link rel="stylesheet" href="<?= asset_url('css/responsive.css') ?>">
 	<title><?= $title ?></title>
 </head>
 <body>
-	
-</body>
-</html>
