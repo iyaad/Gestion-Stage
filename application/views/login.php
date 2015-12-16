fcee482fd50101dfb9fc0257393cc1cc878e41d0
iyaad <iyadhakim@gmail.com>
2015-12-16 23:56:45 +0000

    login.php modifed

--- application/views/login.php
+++ application/views/login.php
@@ -8,5 +8,9 @@
 			<label for="password">Mot de Passe</label>
 			<input type="password" name="password" class="form-control">
 		</div>
+		<div >
+	        <button type="submit" class="btn btn-info waves-effect waves-light">Se Connecter</button>
+	        <a class="pull-right" href="<?= base_url('Users/register') ?>">Creer un Ccompte</a>
+	    </div>
 	</form>
 </div>
