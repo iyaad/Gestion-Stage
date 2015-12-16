cc74331bf4c39a5c9e494a159afb0a3f9409021c
iyaad <iyadhakim@gmail.com>
2015-12-16 23:31:32 +0000

    login.php modified

--- application/views/login.php
+++ application/views/login.php
@@ -2,13 +2,13 @@
 
 	<?= form_open("Users/login") ?> 
 		<div class="form-group">
-			<label class="col sm-1">login</label>
-			<input type="text" class="form-group" value="ici..">
+			<div class="col-sm-2"><label >login</label></div>
+			<div class="col-sm-3"><input type="password" class="form-group" value="ici.."></div>
 		</div>
-
 		<div class="form-group">
-			<label class="col sm-1">password</label>
-			<input type="password" class="form-group" value="ici..">
+			<div class="col-sm-2"><label >password</label></div>
+			<div class="col-sm-3"><input type="password" class="form-group" value="ici.."></div>
 		</div>
+		<div class="col-sm">
 	</form>
 </div>
