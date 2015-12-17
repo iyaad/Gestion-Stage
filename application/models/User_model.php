851381d5ffe88bc0d157df1312257d077ba8f06c
Naoufal <naoufal.touailat@gmail.com>
2015-12-17 23:44:04 +0000

    methode getUser

---
+++ application/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model extends CI_Model {
+
+        public function getUser($id){
+                $query = $this->db->query('select * from user where userId='.$id);
+                return $query->row();
+        }
+}
+
+?>
