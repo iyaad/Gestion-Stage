6d1d5e5e1d347d41a13a9d532ea0fa711a746544
ImadSalimi <salimi.imad@gmail.com>
2015-12-17 23:29:31 +0000

    allowed browser requests to tools controller

--- application/controllers/Tools.php
+++ application/controllers/Tools.php
@@ -5,9 +5,6 @@ class Tools extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		// can only be called from the command line
-		if (!$this->input->is_cli_request()) {
-			exit('Direct access is not allowed. This is a command line tool, use the terminal');
-		}
 		$this->load->dbforge();
 	}
 
