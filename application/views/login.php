9c78f7fcf2fb519bfb27eccd4e2e9e16345b8b32
ImadSalimi <salimi.imad@gmail.com>
2015-12-17 00:02:25 +0000

    Added form errors

--- application/views/login.php
+++ application/views/login.php
@@ -1,16 +1,20 @@
 <div id="login" class="container">
-	<?= form_open("Users/login", 'class="col-sm-4 col-sm-offset-4"') ?> 
-		<div class="form-group">
-			<label for="username">Login</label>
-			<input type="text" name="username" class="form-control">
-		</div>
-		<div class="form-group">
-			<label for="password">Mot de Passe</label>
-			<input type="password" name="password" class="form-control">
-		</div>
-		<div >
-	        <button type="submit" class="btn btn-info waves-effect waves-light">Se Connecter</button>
-	        <a class="pull-right" href="<?= base_url('Users/register') ?>">Creer un Ccompte</a>
-	    </div>
-	</form>
+	<div class="form-container col-sm-4 col-sm-offset-4" >
+		<?= form_open("Users/login") ?> 
+			<div class="form-group">
+				<label for="username">Login</label>
+				<?= form_error('username') ?>
+				<input type="text" name="username" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="password">Mot de Passe</label>
+				<?= form_error('password') ?>
+				<input type="password" name="password" class="form-control">
+			</div>
+			<div >
+		        <button type="submit" class="btn btn-info waves-effect waves-light">Se Connecter</button>
+		        <a class="pull-right" href="<?= base_url('Users/register') ?>">Creer un Ccompte</a>
+		    </div>
+		</form>
+	</div>
 </div>
