f215da778cb141a2b790e61c5cd8e351c535c753
Naoufal <naoufal.touailat@gmail.com>
2015-12-18 00:01:49 +0000

    fixing problem

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,19 +2,12 @@
 
 class User_model extends CI_Model {
 
-<<<<<<< HEAD
+
 	public function getUser($id){
 		$query = $this->db->get_where('User', array('userId' => $id);
 		return $query->row();
 	}
 	
+
 }
 
-?>
-=======
-	public function getUser($id) {
-		$query = $this->db->query('select * from user where userId='.$id);
-		return $query->row();
-	}
-}
->>>>>>> 0de0422591343c9a3d5e2397a744c5a7636a7b23
