fd06a32d94124585cbf9fc2f24198efd598b61c8
ImadSalimi <salimi.imad@gmail.com>
2015-12-18 00:49:29 +0000

    Checking login existance

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -7,11 +7,12 @@ class Users extends MY_Controller {
 		parent::__construct();
 		$this->load->helper('form');
 		$this->load->library('form_validation');
+		$this->load->model('user_model');
 	}
 
 	public function login()
 	{
-		$this->form_validation->set_rules('username', 'Login', 'required|trim');
+		$this->form_validation->set_rules('username', 'Login', 'required|trim|callback_check_username');
 		$this->form_validation->set_rules('password', 'Mot de passe', 'required|trim');
 		$this->form_validation->set_message('required', 'Le champs %s est obligatoire');
 
@@ -21,6 +22,12 @@ class Users extends MY_Controller {
 		} else {
 			echo 'Validé';
 		}
+	}
+
+	public function check_username($username)
+	{
+		$this->form_validation->set_message('check_username', '%s inexistant');
+		return (bool) $this->user_model->getUserByUsername($username);
 	}
 
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,27 +2,28 @@
 
 class User_model extends CI_Model {
 
-	public function getUser($id){
-		$query = $this->db->get_where('User', array('userId' => $id);
+	public function getUser($id) {
+		$query = $this->db->get_where('User', array('userId' => $id));
 		return $query->row();
 	}
-	public function getUserByUsername($username){
-		$query = $this->db->get_where('User', array('username' => $username);
-			return $query->row();
 
+	public function getUserByUsername($username) {
+		$query = $this->db->get_where('User', array('username' => $username));
+		return $query->row();
 	}
 
-	public function createUser($data){
+	public function createUser($data) {
 		return $this->db->insert('User', $data);
 	}
 
-	public function updateUser($id, $data){
+	public function updateUser($id, $data) {
 		$this->db->where('userId', $id);
 		return $this->db->update('User', $data);
 	}
-	public function updatePassword($id,$password){
+
+	public function updatePassword($id, $password) {
 		$this->db->where('userId', $id);
-		return $this->db->update('User',$password);
+		return $this->db->update('User', $password);
 	}
 
 }
