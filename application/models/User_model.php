7075709cf95b30e3aa778a5c6ac62dd080f2440e
Naoufal <naoufal.touailat@gmail.com>
2015-12-18 00:42:42 +0000

    create method getUserByUsername & updatePassword

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -6,6 +6,11 @@ class User_model extends CI_Model {
 		$query = $this->db->get_where('User', array('userId' => $id);
 		return $query->row();
 	}
+	public function getUserByUsername($username){
+		$query = $this->db->get_where('User', array('username' => $username);
+			return $query->row();
+
+	}
 
 	public function createUser($data){
 		return $this->db->insert('User', $data);
@@ -14,6 +19,10 @@ class User_model extends CI_Model {
 	public function updateUser($id, $data){
 		$this->db->where('userId', $id);
 		return $this->db->update('User', $data);
+	}
+	public function updatePassword($id,$password){
+		$this->db->where('userId', $id);
+		return $this->db->update('User',$password);
 	}
 
 }
