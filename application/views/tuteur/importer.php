54a40170d3c972e94eb6fe9b0afc3d1c425f2015
Naoufal <naoufal.touailat@gmail.com>
2015-12-18 20:22:59 +0000

    viem importer created

--- application/views/tuteur/importer.php
+++ application/views/tuteur/importer.php
@@ -1,1 +1,22 @@
-<?
+<div class="wrapper-page">
+	<div class="card-box">
+		<div class="panel-heading"> 
+			<h3 class="text-center"> Importer etudiants <strong class="text-custom"></strong> </h3>
+		</div> 
+		<div class="panel-body">
+			<?= form_open_multipart("tuteur/importer") ?>
+				<div class="form-group ">
+					
+		 					<input type="file" name="file">
+					
+				</div>
+				<div class="form-group text-center m-t-40">
+					<div>
+						<button class="btn btn-pink btn-block text-uppercase waves-effect waves-light" type="submit">Importer</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div><!-- /.card-box -->
+</div>
+
