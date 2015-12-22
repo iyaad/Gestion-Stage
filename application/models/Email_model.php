f3f2e437f598dc6ff67747a543c45c38cba63d11
Naoufal <naoufal.touailat@gmail.com>
2015-12-22 18:57:55 +0000

    html email

--- application/models/Email_model.php
+++ application/models/Email_model.php
@@ -5,20 +5,22 @@ class Email_model extends CI_Model {
 
 	public function emailEtudiant($user) 
 	{
-		$cne = $this->db->get_where('Etudiant', ['etudiantId' => $user->userId])->result()[0]->cne;
+		$user = $this->db->get_where('Etudiant', ['etudiantId' => $user->userId])->result()[0];
+		$data['user'] = $user ;
 		$this->email->from('stages@ensat.ac.ma', 'Chef de filiere');
-		$this->email->to("salimi.imad@gmail.com");
+		$this->email->to($user->email);
 		$this->email->subject('Compte pour la plateforme gestion-stages');
-		$this->email->message('Vous êtes inscris sur gestion-stages avec les paramètres suivants:<br>Login : '.$user->username.'<br>Mot de passe : '.$cne);
+		$this->email->message($this->load->view('email/newAccount'),$data);
 		$this->email->send();
 	}
 	public function recoverPassword($login)
 	{
 		$user = $this->db->get_where('user',['username'=>$login])->result()[0];
+		$data['user'] = $user ;
 		$this->email->from('stages@ensat.ac.ma', 'Chef de filiere');
-		$this->email->to("salimi.imad@gmail.com");
+		$this->email->to($user->email);
 		$this->email->subject('recuperation du mot de passe');
-		$this->email->message('Vous êtes inscris sur gestion-stages avec les paramètres suivants:<br>Login : '.$user->username.'<br>Mot de passe : '.$user->password);
+		$this->email->message($this->load->view('email/passwordRecover'),$data);
 		return (bool) $this->email->send();
 	}
 
