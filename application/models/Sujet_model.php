84bbf71f73f6c9fd234254295ed8f3724a18fd8d
ImadSalimi <salimi.imad@gmail.com>
2016-01-04 23:56:19 +0000

    Fixed merges

--- application/models/Sujet_model.php
+++ application/models/Sujet_model.php
@@ -36,34 +36,19 @@ class Sujet_model extends CI_Model {
 		$this->db->where($criteria);
 		return $this->db->get()->row();
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 	public function aPostule($sujet,$etudiant){
-
 		$data = array(
 			'sujetId' => $sujet,
 			'etudiantId' => $etudiant
-			);
+		);
 		$this->db->where($data);
 		$res = $this->db->get('Postulat')->result();
-		return count($res) > 0 ;
-=======
-	public function nbreSujets($id)
-	{
-		$this->load->model('etudiant_model');
-		$etudiant = $this->etudiant_model->getEtudiant(array('etudiantId'=>$id));
-		$this->db->select('e.*');
-		$this->db->from('etudiant e');
-		$this->db->where('niveau',$etudiant->filiere);
-		$this->db->where('filiere',$etudiant->niveau);
-		return $this->db->count_all_results();
->>>>>>> 6a7467d0035cc0bad55a5acf6189744d83f3b5ec
-=======
+		return count($res) > 0;
+	}
 
 	public function updateSujet($criteria, $data) {
 		$this->db->where($criteria);
 		return $this->db->update('Sujet', $data);
->>>>>>> bd31f1ae789fbc452171a6c2d25d7e2f25fb9028
 	}
 }
